29274043f597929b5350b617913a86bf93340746
rajmeen-fatima <rajmeenf@gmail.com>
2026-09-24 17:21:06 +0500

    Rename class from Main to jenkins and update output

--- jenkins.java
+++ jenkins.java
@@ -1,5 +1,5 @@
-public class Main {
+public class jenkins {
     public static void main(String[] args) {
-        System.out.println("Hello, World! ver 2 ");
+        System.out.println("Hello, World!");
     }
 }
